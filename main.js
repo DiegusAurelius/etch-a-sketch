16f9465cdeb56bf248cff80947b20066a75dcaea
DiegusAurelius <diegusaurelius@gmail.com>
2025-03-08 07:47:43 -0600

    Add createGrid() and createRow()

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('It works!');
+const canvas = document.querySelector('.etch-a-sketch');
+
+let squares = 4;
+
+function createGrid(squares) {
+  createRow;
+}
+
+let createRow = (squares) => {
+  for (let i = 0; i <= squares; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    canvas.appendChild(square);
+  }
+};
+createGrid(squares);
