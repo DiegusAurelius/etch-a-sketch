dc6facec0e3c9655e4f0ca83a5db4d475f1b0477
DiegusAurelius <diegusaurelius@gmail.com>
2025-03-09 06:27:39 -0600

    Fix: move  event listener for better performance

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 const canvas = document.querySelector('.canvas');
 const slider = document.querySelector('#slider');
 const sliderValue = document.querySelector('#slider-value');
+let light = 100;
 
 sliderValue.textContent = slider.value;
 
 slider.addEventListener('input', (e) => {
   sliderValue.textContent = e.target.value;
+  light = 100;
+  canvas.innerHTML = '';
   createSquares(e.target.value);
+});
+
+canvas.addEventListener('mouseover', (e) => {
+  e.target.style.backgroundColor = `hsl(${
+    Math.floor(Math.random() * 360) + 1
+  }, 100%, ${(light -= 10)}%)`;
 });
 
 function setSquareSize(value) {
@@ -14,9 +23,6 @@ function setSquareSize(value) {
 }
 
 function createSquares(value) {
-  canvas.innerHTML = '';
-
-  let light = 100;
   let squareSize = setSquareSize(value);
 
   for (let i = 0; i < value ** 2; i++) {
@@ -26,12 +32,6 @@ function createSquares(value) {
     square.style.height = `${squareSize}px`;
 
     canvas.appendChild(square);
-
-    square.addEventListener('mouseover', (e) => {
-      e.target.style.backgroundColor = `hsl(${
-        Math.floor(Math.random() * 360) + 1
-      }, 100%, ${(light -= 10)}%)`;
-    });
   }
 }
 
